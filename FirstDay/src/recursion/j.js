2cb9494450b0006cf3b05dade6c7222cc8d1fcd6
nitheshurao <nitheshurao@gmail.com>
2024-06-24 21:08:52 +0530

    setup

--- FirstDay/src/recursion/j.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function j() {
-    log
-  return (
-    <div>j</div>
-  )
-}
--- MYCODE/MYCODE/src/Recursion/D1one.java
+++
@@ -1,9 +0,0 @@
-package Recursion;
-
-public class D1one {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        // d
-        // name
-    }
-}
